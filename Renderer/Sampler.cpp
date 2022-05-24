5288b6bb4f185682b6a2e643f61c76e373022a40
tian <just.tzh@outlook.com>
2022-05-24 21:40:23 +0800

    增加随机均匀采样器

--- Renderer/Sampler.cpp
+++ Renderer/Sampler.cpp
@@ -4,21 +4,29 @@
 #include "Sampler.h"
 
 Vector2d Sampler::CastRayByDistribution(int x, int y) {
-    index = index >= 4 ? 0 : index;
+    double s = x, t = y;
 
-    int sx = index % 2;
-    int sy = index / 2;
+    if(type == SamplerType::Trapezoidal){
+        index = index >= 4 ? 0 : index;
 
-    double r1 = 2 * RandomDouble();
-    double r2 = 2 * RandomDouble();
+        int sx = index % 2;
+        int sy = index / 2;
 
-    double dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
-    double dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
+        double r1 = 2 * RandomDouble();
+        double r2 = 2 * RandomDouble();
 
-    double s = static_cast<double>(x) + (dx + sx + 0.5) / 2;
-    double t = static_cast<double>(y) + (dy + sy + 0.5) / 2;
+        double dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
+        double dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
 
-    index++;
+        s = static_cast<double>(x) + (dx + sx + 0.5) / 2;
+        t = static_cast<double>(y) + (dy + sy + 0.5) / 2;
+
+        index++;
+    }
+    else if(type == SamplerType::Uniform){
+        s = static_cast<double>(x) + RandomDouble();
+        t = static_cast<double>(y) + RandomDouble();
+    }
 
     return Vector2d(s, t);
 }
--- Renderer/Sampler.h
+++ Renderer/Sampler.h
@@ -6,10 +6,17 @@
 #include "../Common/Global.h"
 #include "../Common/Vector.h"
 
+//Distribution Type
+enum SamplerType{
+    Trapezoidal,
+    Uniform
+};
+
 struct Sampler {
     int index = 0;
+    SamplerType type;
 
-    Sampler() {}
+    Sampler(SamplerType _type):type(_type) {}
 
     Vector2d CastRayByDistribution(int x, int y);
 };
