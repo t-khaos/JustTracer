246fc8874682f8f002ab362b031fe3dc3beaed48
tian <just.tzh@outlook.com>
2022-05-27 23:53:38 +0800

    fix

--- Math/Global.h
+++ Math/Global.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define PI 3.1415926f
+#define PI 3.14159265359f
 #define EPSILON 0.001f
 #define MAX_FLOAT std::numeric_limits<float>::max()
 #define MIN_FLOAT std::numeric_limits<float>::min()
