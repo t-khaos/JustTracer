77fe833846c6be3000d64bb220207fa0a5a3043e
tian <just.tzh@outlook.com>
2022-06-01 01:31:52 +0800

    改进射线与包围盒相交

--- Accelerator/AABB.cpp
+++ Accelerator/AABB.cpp
@@ -2,8 +2,8 @@
 #include "AABB.h"
 
 bool AABB::Intersect(const Ray &ray, float t_near) {
-
     Vector3 invDirection = 1.0f / ray.direction;
+
     Vector3 t0 = (minVector - ray.origin) * invDirection;
     Vector3 t1 = (maxVector - ray.origin) * invDirection;
 
@@ -13,5 +13,13 @@ bool AABB::Intersect(const Ray &ray, float t_near) {
     float t_enter = MaxValue(t_min);
     float t_exit = MinValue(t_max);
 
-    return t_enter <= t_exit && t_exit > 0;
+    //射线与包围盒不相交
+    if(t_enter>t_exit+0.00016f | t_exit < 0.0f)
+        return false;
+
+    //射线已经和别的包围盒相交
+    if(t_enter < t_near)
+        return false;
+
+    return true;
 }
